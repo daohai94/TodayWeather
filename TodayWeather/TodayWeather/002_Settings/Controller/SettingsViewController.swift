72ea3d72d012409e5905188912889edeafaed6cf
Dao Thanh Hai <hai.daothanh@vti.com.vn>
2020-03-19 15:39:39 +0700

    fix ui menu

---
+++ TodayWeather/TodayWeather/002_Settings/Controller/SettingsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewController.swift
+//  TodayWeather
+//
+//  Created by Dao Thanh Hai on 3/19/20.
+//  Copyright © 2020 Đào Thanh Hải. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
