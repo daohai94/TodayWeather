cee4effd7e9a96dbed9db4f37921596de28889d8
Tuan Vu <shjn@shjn.local>
2020-04-28 11:09:33 +0700

    update pod

--- TodayWeather/TodayWeather/004_Home/Views/WeatherCollectionViewCell.swift
+++ TodayWeather/TodayWeather/004_Home/Views/WeatherCollectionViewCell.swift
@@ -7,12 +7,31 @@
 //
 
 import UIKit
-
+import UICircularProgressRing
 class WeatherCollectionViewCell: UICollectionViewCell {
 
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var sunSetView: UIView!
+
+    let progressRing = UICircularTimerRing()
     override func awakeFromNib() {
         super.awakeFromNib()
+
+//        progressRing.maxValue = 100
+//        progressRing.value = 30
+        progressRing.style = .dashed(pattern: [7.0, 7.0])
+        
+        progressRing.backgroundColor = .red
+        sunSetView.addSubview(progressRing)
+
+        progressRing.translatesAutoresizingMaskIntoConstraints = false
+        let leadingConstraint = NSLayoutConstraint(item: progressRing, attribute: NSLayoutConstraint.Attribute.leading, relatedBy: NSLayoutConstraint.Relation.equal, toItem: sunSetView, attribute: NSLayoutConstraint.Attribute.leading, multiplier: 1, constant: 0)
+        let topConstraint = NSLayoutConstraint(item: progressRing, attribute: NSLayoutConstraint.Attribute.top, relatedBy: NSLayoutConstraint.Relation.equal, toItem: sunSetView, attribute: NSLayoutConstraint.Attribute.top, multiplier: 1, constant: 0)
+        let traillingConstraint = NSLayoutConstraint(item: progressRing, attribute: NSLayoutConstraint.Attribute.trailing, relatedBy: NSLayoutConstraint.Relation.equal, toItem: sunSetView, attribute: NSLayoutConstraint.Attribute.trailing, multiplier: 1, constant: 0)
+        let bottomConstraint = NSLayoutConstraint(item: progressRing, attribute: NSLayoutConstraint.Attribute.bottom, relatedBy: NSLayoutConstraint.Relation.equal, toItem: sunSetView, attribute: NSLayoutConstraint.Attribute.bottom, multiplier: 1, constant: 0)
+        sunSetView.addConstraints([leadingConstraint, topConstraint, traillingConstraint, bottomConstraint])
+
+
         slider.setThumbImage(UIImage(named: "dot"), for: .normal)
         slider.setThumbImage(UIImage(named: "dot"), for: .highlighted)
     }
